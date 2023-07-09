cbea08ff25a8406af7fea931c5ec446011be74b0
Dylan-Ay <dylan.ayache.68@gmail.com>
2023-07-09 17:31:35 +0200

    Transformation composant seeMoreButton en link

--- src/app/components/SeeMoreButton.tsx
+++ src/app/components/SeeMoreButton.tsx
@@ -1,15 +1,18 @@
 import { ReactNode, useState } from "react";
+import Link from "next/link";
 
 interface Props {
   children: ReactNode;
   color?: 'primary' | 'secondary' | 'danger' | 'success';
-  // onClick: () => void;
+  href?: string;
 }
 
-const SeeMoreButton = ({ children, color = 'primary' }: Props) => {
-  return(
-  <button type="button" className={"btn btn-" + color }>{children}</button>
-  );
+const SeeMoreButton = ({ children, color = 'primary', href }: Props) => {
+  if (href) {
+    return (
+      <Link href={href} passHref className={"btn btn-" + color}>{children}</Link>
+    )
+  }
 };
 
 export default SeeMoreButton;
