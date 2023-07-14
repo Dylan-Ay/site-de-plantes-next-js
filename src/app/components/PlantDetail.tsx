04e2b3c6586e7e86ee1af252a9ee4bfff70228c0
Dylan-Ay <dylan.ayache.68@gmail.com>
2023-07-14 15:55:03 +0200

    Ajout contenu PlantDetail

--- src/app/components/PlantDetail.tsx
+++ src/app/components/PlantDetail.tsx
@@ -1,3 +1,5 @@
+import { calculNbEmoji } from "../utils";
+
 interface Props {
     img: string;
     name: string;
@@ -10,12 +12,19 @@ interface Props {
 function PlantDetail({img, name, category, light, water, fullDescription}: Props) {
   return (
     <section>
-        <h1 className="py-3 text-center">{name}</h1>
+        <h1 className="py-3 text-center">{(name.charAt(0).toLocaleUpperCase() + name.slice(1))}</h1>
         <img className="img-fluid" src={img} alt={"photo d'une plante " + name} />
         
 
         <h3 className="pt-4">Description :</h3>
         <p>{fullDescription}</p>
+
+        <h3>Caractéristiques :</h3>
+        <ul>
+          <li>Besoin en eau: {calculNbEmoji(water, "💧")}</li>
+          <li>Besoin en lumière: {calculNbEmoji(light, "☀️")}</li>
+          <li>Catégorie: {category}</li>
+        </ul>
     </section>
   )
 }
