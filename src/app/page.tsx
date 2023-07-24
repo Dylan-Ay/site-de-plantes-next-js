73cd7a093ac6e763e402edaa0a2b278ee0fffdf5
Dylan-Ay <dylan.ayache.68@gmail.com>
2023-07-24 22:15:43 +0200

    Modification espacements des éléments page d'accueil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <main>
       <div className="container mx-auto pb-9">
         <h1 className="py-10 text-5xl text-center">Liste des Plantes</h1>
-        <div className="flex flex-wrap justify-evenly gap-1 gap-y-10">
+        <div className="flex flex-wrap justify-center gap-10">
           {plants.map((plant) => (
             <PlantCard
               key={plant.id}
