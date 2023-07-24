7064bbee8d518a1530027af36cf93dce344caf38
Dylan-Ay <dylan.ayache.68@gmail.com>
2023-07-24 22:16:14 +0200

    Ajout class tailwind PlantDetail

--- src/components/PlantDetail.tsx
+++ src/components/PlantDetail.tsx
@@ -9,18 +9,18 @@ interface Props {
     fullDescription: string;
   }
 
-function PlantDetail ({img, name, category, light, water, fullDescription}: Props) {
+export default function PlantDetail ({img, name, category, light, water, fullDescription}: Props) {
   return (
     <section>
         <h1 className="py-10 text-5xl text-center">{firstLetterToCapitalize(name)}</h1>
         <div className="w-11/12 md:w-9/12 lg:w-7/12 mx-auto bg-white p-5 rounded-md">
-          <img src={img} alt={"photo d'une plante " + name} />
+          <img className="mx-auto" src={img} alt={"photo d'une plante " + name} />
           
           <h2 className="pt-6 pb-2 font-semibold text-xl">Description :</h2>
           <p className="pb-5">{fullDescription}</p>
 
           <h3 className="font-semibold text-lg pb-2">Caractéristiques :</h3>
-          <ul>
+          <ul className="leading-7">
             <li>Besoin en eau: {calculNbEmoji(water, "💧")}</li>
             <li>Besoin en lumière: {calculNbEmoji(light, "☀️")}</li>
             <li>Catégorie: {category}</li>
@@ -29,4 +29,3 @@ function PlantDetail ({img, name, category, light, water, fullDescription}: Prop
     </section>
   )
 }
-export default PlantDetail
