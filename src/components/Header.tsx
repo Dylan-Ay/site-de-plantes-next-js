f8902654aa7f0e5e4dbf551ee0726a160291b38c
Dylan-Ay <dylan.ayache.68@gmail.com>
2023-07-24 15:07:53 +0200

    Update class de la nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ export default function Example() {
 
   return (
     <header className="bg-white">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <nav className="container flex mx-auto items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
