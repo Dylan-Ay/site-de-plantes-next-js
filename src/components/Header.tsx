b4a0ceae861f6ec8251c4a5b5c4f2b03137d176e
Dylan-Ay <dylan.ayache.68@gmail.com>
2023-07-25 17:01:10 +0200

    Suppression "import Image" inutilisée

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,6 @@
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import Image from 'next/image'
 
 const navigation = [
   { name: 'Liste des Plantes', href: '/' },
