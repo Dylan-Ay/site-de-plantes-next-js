67c78324c5bacfdc8c7f5ad94a0dd04e1f389319
Dylan-Ay <dylan.ayache.68@gmail.com>
2023-07-26 15:04:49 +0200

    Ajout d'un système de filtre par catégorie de plante + rendu dynamique des catégories lors de l'affichage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,41 @@ import SeeMoreButton from '@/components/SeeMoreButton'
 import PlantCard from '@/components/PlantCard'
 
 export default function Home() {
-  const [plants, setPlant] = useState(plantList)
+  const [plants, setPlants] = useState(plantList);
   
+  // Copie du tableau de plantes
+  const plantsCopied = [...plantList];
+
+  // Obtient une liste unique de toutes les catégories de plantes
+  const plantsCategories = Array.from(new Set(plantsCopied.map((plant) => plant.category)));  
+
+  // Ajout de la catégorie "Toutes" à la liste
+  plantsCategories.unshift('Toutes');
+    
+  const handleFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const category = event.target.value;
+
+    if (category === "Toutes") {
+      setPlants(plantsCopied);
+    } else {
+      // Garde les plantes dont la catégorie est celle choisie
+      const filteredPlants = plantsCopied.filter((plant) => plant.category === category);
+      setPlants(filteredPlants);
+    }
+  }
+
   return (
     <main>
       <div className="container mx-auto pb-9">
         <h1 className="py-10 text-5xl text-center">Liste des Plantes</h1>
+        <div>
+          {plantsCategories.map((category) => 
+            <>
+              <label key={category} htmlFor="filter">{category}</label>
+              <input type="radio" name="filter" value={category} onChange={handleFilter} />
+            </>
+          )}
+        </div>
         <div className="flex flex-wrap justify-center gap-10">
           {plants.map((plant) => (
             <PlantCard
