dfa27b33b46c3d0dee43a70b58e0a6afba44832a
Dylan-Ay <dylan.ayache.68@gmail.com>
2023-07-27 17:27:02 +0200

    Système de filtre rendu dynamique + création de fonctions + ajout d'un paramètre keyValue à handleFilter + ajout d'un filtre par besoin en eau

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,20 +11,47 @@ export default function Home() {
   // Copie du tableau de plantes
   const plantsCopied = [...plantList];
 
-  // Obtient une liste unique de toutes les catégories de plantes
-  const plantsCategories = Array.from(new Set(plantsCopied.map((plant) => plant.category)));  
+  // Permet de récupérer un tableau filtré par une certaine propriété
+  const createArrayOfFilterable = (elementsArray: Array<any>, keyValue: string, elementToUnshift: string, sort : boolean = false ) => {
+    const plantsElements = Array.from(new Set(elementsArray.map((plant) => plant[keyValue])));
 
-  // Ajout de la catégorie "Toutes" à la liste
-  plantsCategories.unshift('Toutes');
-    
-  const handleFilter = (category: string) => {
+    plantsElements.unshift(elementToUnshift);
+    if (sort) {
+      plantsElements.sort((a,b) => b - a)
+    }
 
-    if (category === "Toutes") {
+    return plantsElements
+  }
+
+  // Permet d'appliquer le filtre sur la liste de plantes par une certaine propriété
+  const applyFilter = (elementsArray: Array<any>, keyValue: string | number, filteredKeyValue : string | number) => {
+    const filteredPlants = elementsArray.filter((plant) => plant[keyValue] === filteredKeyValue) 
+
+    return filteredPlants;
+  }
+  // Création des tableaux filtrés
+  const plantsCategories = createArrayOfFilterable(plantsCopied, 'category', 'Toutes');
+  const plantsWaterNeed = createArrayOfFilterable(plantsCopied, 'water', 'Tous', true);
+  
+  // Element d'écoute sur le fitre
+  const handleFilter = (value: string | number, keyValue: string) => {
+
+    if (value === "Toutes" || value === "Tous") {
       setPlants(plantsCopied);
-    } else {
-      // Garde les plantes dont la catégorie est celle choisie
-      const filteredPlants = plantsCopied.filter((plant) => plant.category === category);
-      setPlants(filteredPlants);
+    }else{
+      switch (keyValue) {
+        case 'water':
+          setPlants(applyFilter(plantsCopied, 'water', value));
+          break;
+  
+        case 'category':
+          setPlants(applyFilter(plantsCopied, 'category', value));
+          break;
+
+        default:
+          setPlants(plantsCopied);
+          break;
+      } 
     }
   }
 
@@ -32,8 +59,10 @@ export default function Home() {
     <main>
       <div className="container mx-auto pb-9">
         <h1 className="py-10 text-5xl text-center">Liste des Plantes</h1>
-        <div className='flex justify-end'>
-          <DropdownFilter categories={plantsCategories} handleFilter={handleFilter}></DropdownFilter>
+        <div className='flex px-8 justify-start mb-12 items-center gap-5'>
+          <span>Filtrer par :</span>
+          <DropdownFilter keyValue='category' filterTitle='Catégories' elementsList={plantsCategories} handleFilter={handleFilter}></DropdownFilter>
+          <DropdownFilter keyValue='water' filterTitle='Besoin en eau' elementsList={plantsWaterNeed} handleFilter={handleFilter}></DropdownFilter>
         </div>
         <div className="flex flex-wrap justify-center gap-10">
           {plants.map((plant) => (
