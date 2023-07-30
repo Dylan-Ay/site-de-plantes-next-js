27f6f524f2dffb4eb72724bbd44eec0224a94479
Dylan-Ay <dylan.ayache.68@gmail.com>
2023-07-30 14:49:30 +0200

    Update css + indentation + ajout du Props handleSort()

--- src/components/SelectSort.tsx
+++ src/components/SelectSort.tsx
@@ -1,18 +1,22 @@
-import { Fragment, useState } from 'react'
-import { Listbox, Transition } from '@headlessui/react'
-import { CheckIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
+import { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
-const sortType = [
-    { id: 1, name: 'Popularité' },
-    { id: 2, name: 'Nom' }
-  ]
-
-function classNames(...classes: any[]) {
-  return classes.filter(Boolean).join(' ')
+interface Props {
+  handleSort: (value: string) => void;
 }
 
-export default function SelectSort() {
-  const [selected, setSelected] = useState(sortType[0])
+const sortType = [
+  { id: 1, name: "Popularité" },
+  { id: 2, name: "Nom" },
+];
+
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function SelectSort({ handleSort }: Props) {
+  const [selected, setSelected] = useState(sortType[0]);
 
   return (
     <Listbox value={selected} onChange={setSelected}>
@@ -22,47 +26,46 @@ export default function SelectSort() {
             <Listbox.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 py-1.5 pl-3 pr-10">
               <span className="block truncate">{selected.name}</span>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-              <ChevronDownIcon
-            className="-mr-1 h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
+                <ChevronDownIcon
+                  className="-mr-1 h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
               </span>
             </Listbox.Button>
 
             <Transition
               show={open}
               as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
             >
               <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-fit overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                 {sortType.map((sort) => (
                   <Listbox.Option
                     key={sort.id}
+                    onClick={() => handleSort(sort.name)}
                     className={({ active }) =>
                       classNames(
-                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                        'relative cursor-pointer select-none py-2 px-4'
+                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                        "relative cursor-pointer select-none py-2 px-4"
                       )
                     }
                     value={sort}
                   >
-                    {({ selected, active }) => (
+                    {({ selected }) => (
                       <>
-                        <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+                        <span
+                          className={classNames(
+                            selected ? "font-semibold" : "font-normal",
+                            "block truncate"
+                          )}
+                        >
                           {sort.name}
                         </span>
-
-                        {selected ? (
-                          <span
-                            className={classNames(
-                              active ? 'text-white' : 'text-indigo-600',
-                              'absolute inset-y-0 right-0 flex items-center pr-4'
-                            )}
-                          >
-                          </span>
-                        ) : null}
                       </>
                     )}
                   </Listbox.Option>
@@ -73,5 +76,5 @@ export default function SelectSort() {
         </>
       )}
     </Listbox>
-  )
+  );
 }
