562225b6252ef34b6c73bca80ea4e21a0527cb4b
Dylan-Ay <dylan.ayache.68@gmail.com>
2023-08-05 14:38:34 +0200

    Refactorisation + nettoyage du code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,28 +20,22 @@ export default function Home() {
     description: string;
     fullDescription: string;
   };
-
+  // useState concernant les listes de plantes
   const [plants, setPlants] = useState(plantList.sort((a, b) => (a.id < b.id) ? 1 : -1));
   const plantsCopied = [...plantList];
   const resetPlantsList = () => {
-    if (activeSort === "Nom") {
-      setPlants(plantsCopied.sort((a, b) => a.name > b.name ? 1 : -1));
-    }else if (activeSort === "Popularité"){
-      setPlants(plantsCopied.sort((a, b) => a.isBestSale < b.isBestSale ? 1 : -1));
-    }else{
-      setPlants(plantsCopied);
-    }
+    applyActiveSort(plantsCopied, activeSort);
     setFilteredPlants(plantsCopied);
   }
   const [resultNumber, setResultNumber] = useState(plantList.length);
   const [filteredPlants, setFilteredPlants] = useState<Plant[]>([]);
 
-  // Met à jour le nombre de résultats à chaque fois que plants change.
+  // Met à jour le nombre de résultats à chaque fois que la liste de plantes change.
   useEffect(() => {
     setResultNumber(plants.length);
   }, [plants]);
   
-  // Permet de récupérer un tableau filtré par une certaine propriété
+  // Permet de  créer un tableau de filtres à partir d'une propriété d'un tableau de plantes
   const createArrayOfFilterable = (elementsArray: Array<any>, keyValue: string, elementToUnshift: string, sort : boolean = false ) => {
     const plantsElements = Array.from(new Set(elementsArray.map((plant) => plant[keyValue])));
 
@@ -53,7 +47,22 @@ export default function Home() {
     return plantsElements
   }
 
-  // Permet d'appliquer le filtre sur la liste de plantes par une certaine propriété
+  // Permet de vérifier si un tri est actif et d'appliquer le tri à un tableau de plantes
+  const applyActiveSort = (elementsArray: Array<any>, activeSort: string) => {
+    switch (activeSort) {
+      case "Nom":
+        setPlants(elementsArray.sort((a, b) => a.name > b.name ? 1 : -1));
+        break;
+      case "Popularité":
+        setPlants(elementsArray.sort((a, b) => a.isBestSale < b.isBestSale ? 1 : -1));
+        break;
+      case "Récent":
+        setPlants(elementsArray.sort((a, b) => a.id < b.id ? 1 : -1));
+        break;
+    }
+  }
+
+  // Permet d'appliquer un filtre sur une liste de plantes par une certaine propriété
   const applyFilter = (elementsArray: Array<any>, keyValue: string, filteredKeyValue : string | number) => {
     switch (activeSort) {
       case "Nom":
@@ -71,12 +80,12 @@ export default function Home() {
     }
   }
 
-  // Création des tableaux filtrés
+  // Création des tableaux de choix de filtres par propriété
   const plantsCategories = createArrayOfFilterable(plantsCopied, 'category', 'Toutes');
   const plantsWaterNeed = createArrayOfFilterable(plantsCopied, 'water', 'Tous', true);
   const plantsLightNeed = createArrayOfFilterable(plantsCopied, 'light', 'Tous', true);
 
-  // Créations des useState
+  // Créations des useState liés aux filtres et au tri
   const [filterValue, setFilterValue] = useState<string | number | null>(null);
   const [filterTitle, setFilterTitle] = useState<string | null>(null);
   const [isFilterActive, setIsFilterActive] = useState<boolean>(false);
@@ -85,13 +94,12 @@ export default function Home() {
   
   // Récupère du composant DropDownFilter la clé et la valeur à partir desquels appliquer le filtre
   const handleFilter = (value: string | number, keyValue: string) => {
-
       setFilterValue(value);
       setIsFilterActive(true);
       setKeyValue(keyValue);
 
     if (value === "Toutes" || value === "Tous") {
-      setPlants(plantsCopied);
+      applyActiveSort(plantsCopied, activeSort);
       setFilteredPlants(plantsCopied);
       setIsFilterActive(false);
     }else{
@@ -102,21 +110,18 @@ export default function Home() {
           setFilteredPlants(waterFiltered);
           setFilterTitle('Arrosage');
           break;
-  
         case 'category':
           const categoryFiltered = applyFilter([...plantsCopied], keyValue, value);
           setPlants(categoryFiltered);
           setFilteredPlants(categoryFiltered);
           setFilterTitle('Catégories');
           break;
-
         case 'light':
           const lightFiltered = applyFilter([...plantsCopied], keyValue, value);
           setPlants(lightFiltered);
           setFilteredPlants(lightFiltered);
           setFilterTitle('Exposition');
           break;
-
         default:
           setPlants(plantsCopied);
           setFilteredPlants(plantsCopied);
@@ -124,55 +129,20 @@ export default function Home() {
       } 
     }
   }
-  // Récupère du composant SelectSort la valeur au clique de la liste déroulante
+  // Récupère du composant SelectSort la valeur au clique de la liste déroulante de tri
   const handleSort = (value: string) => {
+    // Si un filtre a été appliqué
     if ([...filteredPlants].length > 0) {
-      switch (value) {
-        case "Popularité":
-          const plantsByPopularity = [...filteredPlants].sort((a, b) =>
-            a.isBestSale < b.isBestSale ? 1 : -1
-          );
-          setPlants(plantsByPopularity);
-          console.log(value + "filtered")
-          break;
-        case "Nom":
-          const plantsByName = [...filteredPlants].sort((a, b) =>
-            a.name > b.name ? 1 : -1
-          );
-          setPlants(plantsByName);
-          break;
-        default:
-          setPlants([...filteredPlants].sort((a, b) => (a.id < b.id ? 1 : -1)));
-          break;
-      }
+      applyActiveSort([...filteredPlants], value);
     } else {
-      switch (value) {
-        case "Popularité":
-          const plantsByPopularity = plantsCopied.sort((a, b) =>
-            a.isBestSale < b.isBestSale ? 1 : -1
-          );
-          console.log(value)
-          setPlants(plantsByPopularity);
-          break;
-        case "Nom":
-          const plantsByName = plantsCopied.sort((a, b) =>
-            a.name > b.name ? 1 : -1
-          );
-          setPlants(plantsByName);
-          break;
-        default:
-          setPlants(plantsCopied.sort((a, b) => (a.id < b.id ? 1 : -1)));
-          break;
-      }
+      applyActiveSort(plantsCopied, value);
     }
     setActiveSort(value);
-    
   };
 
   return (
     <main className='container mx-auto pb-9'>
         <h1 className="py-10 text-5xl text-center">Liste des Plantes</h1>
-
         <section className='bg-white rounded-md mb-12 px-8 mx-auto max-w-[1041px]'>
           <div className='flex items-center gap-7 pt-4'>
             <span>Trier par :</span>
